16cb307d67c60c93afbed71f2019963c97c8bc3d
Raghunandan <vadlaraghunandan@gmail.com>
2020-01-31 16:57:22 +0530

    Hello 31-01-2020..commited

---
+++ app/src/main/java/app/actionnation/actionapp/admin/AdminCommonClass.java
@@ -0,0 +1,50 @@
+package app.actionnation.actionapp.admin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.MenuItem;
+
+import app.actionnation.actionapp.Admin_Challenges;
+import app.actionnation.actionapp.Admin_Education;
+import app.actionnation.actionapp.CertificateGenerator;
+import app.actionnation.actionapp.DbMainActivity;
+import app.actionnation.actionapp.DetailsActivity;
+import app.actionnation.actionapp.R;
+import app.actionnation.actionapp.adminleadership;
+
+public class AdminCommonClass {
+
+    public MenuItem AdminMenuSelected(MenuItem item, Context ct) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.ad_category:
+                Intent homepage = new Intent(ct, DbMainActivity.class);
+                ct.startActivity(homepage);
+                break;
+            case R.id.ad_Education:
+                homepage = new Intent(ct, Admin_Education.class);
+                ct.startActivity(homepage);
+                break;
+            case R.id.ad_Challenges:
+                homepage = new Intent(ct, Admin_Challenges.class);
+                ct.startActivity(homepage);
+                break;
+            case R.id.ad_Leadership:
+                homepage = new Intent(ct, adminleadership.class);
+                ct.startActivity(homepage);
+                break;
+            case R.id.ad_setImage:
+                homepage = new Intent(ct, CertificateGenerator.class);
+                ct.startActivity(homepage);
+                break;
+            case R.id.ad_editAdmin:
+                homepage = new Intent(ct, DetailsActivity.class);
+                ct.startActivity(homepage);
+                break;
+        }
+        return item;
+    }
+
+
+}

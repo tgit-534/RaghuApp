76236740dd4295d94516adc6d304b7cb5226eac9
Raghunandan <vadlaraghunandan@gmail.com>
2020-01-31 17:26:56 +0530

    Hello 31-01-2020..commited

---
+++ app/src/main/java/app/actionnation/actionapp/Database_Content/UserCourse.java
@@ -0,0 +1,37 @@
+package app.actionnation.actionapp.Database_Content;
+
+public class UserCourse {
+    String Edu_Id;
+    int Challenge_Number;
+    String Fb_Id;
+
+
+    public UserCourse()
+{
+
+}
+
+    public String getEdu_Id() {
+        return Edu_Id;
+    }
+
+    public void setEdu_Id(String edu_Id) {
+        Edu_Id = edu_Id;
+    }
+
+    public int getChallenge_Number() {
+        return Challenge_Number;
+    }
+
+    public void setChallenge_Number(int challenge_Number) {
+        Challenge_Number = challenge_Number;
+    }
+
+    public String getFb_Id() {
+        return Fb_Id;
+    }
+
+    public void setFb_Id(String fb_Id) {
+        Fb_Id = fb_Id;
+    }
+}
